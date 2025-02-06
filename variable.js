bc2dcda5f4e71db4d3bbec66bef7c33ca6f62677
Ziead Ahmed <zyeadahmed629@gmail.com>
2025-02-06 22:04:56 +0200

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,21 @@
+/*
+alert("welcome Zyead")
+*/
+//types variable(js).
+var x; //golobal Var
+        console.log(typeof x); // undefiend
+        x=10;
+        console.log(typeof x); // number
+        x=true;
+        console.log(typeof x);  // boolean
+        x="zyead";
+        console.log(typeof x); // string
+        x="10";
+        console.log(typeof x); // string
+        x="";
+        console.log(typeof x); //string
+        x=null;
+        console.log(typeof x); // object
+        console.log(x);   // NULL
+
+  
